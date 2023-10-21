dff298084fe73495dca806f493eeab15d9bce238
Elodie <anthonyelodie@gmail.com>
2023-10-21 18:04:35 +0200

    Add testimony content

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-var slider = $('.slider');
 var nextButton = $('.slider__next');
 var prevButton = $('.slider__prev');
 
@@ -36,24 +35,34 @@ $('.slider').slick({
   nextArrow: nextButton,
 });
 
-// var isPrevActive = false;
-
-// slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-//   isPrevActive = nextSlide < currentSlide;
-// });
-
-// slider.on('afterChange', function (event, slick, currentSlide) {
-//   if (isPrevActive) {
-//     prevButton.addClass('active');
-//     nextButton.removeClass('active');
-//   } else {
-//     prevButton.removeClass('active');
-//     nextButton.addClass('active');
-//   }
-// });
-
-// prevButton.on('click', function () {
-//   isPrevActive = true;
-//   prevButton.addClass('active');
-//   nextButton.removeClass('active');
-// });
+$('.slider-testimony').slick({
+  dots: true,
+//   infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+  prevArrow: prevButton,
+  nextArrow: nextButton,
+});
