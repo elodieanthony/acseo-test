901e0fd21ba4641ef4aa00780f14da1914f442c1
Elodie <anthonyelodie@gmail.com>
2023-10-22 16:49:28 +0200

    Update realization slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,56 @@
-var nextButton = $('.slider__next');
-var prevButton = $('.slider__prev');
+$(document).ready(function () {
+  // Slick slider launch
 
-$('.slider').slick({
-  arrows: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+  //Slider realization
+  $('.slider').slick({
+    arrows: true,
+    slidesToShow: 1.5,
+    slidesToScroll: 2,
+    centerMode: true,
+    centerPadding: '20%',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
       },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
-  prevArrow: prevButton,
-  nextArrow: nextButton,
-});
+    ],
+    prevArrow: $('.slider__prev'),
+    nextArrow: $('.slider__next'),
+  });
 
-$('.slider-testimony').slick({
-  dots: true,
-  arrows: false,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+  // Slider testimony
+  $('.slider-testimony').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
       },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
+    ],
+  });
 });
